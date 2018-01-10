3a91ed4a242cdf030c0ba91e6f4cb2089c0f8bb8
Tensor-Programming <abeltensor@tensor-programming.com>
2018-01-09 21:40:06 -0500

    initial commit

---
+++ lib/graphical/accounts/user.ex
@@ -0,0 +1,21 @@
+defmodule Graphical.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Graphical.Accounts.User
+
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    has_many :posts, Graphical.Posts.Post
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:name, :email])
+    |> validate_required([:name, :email])
+  end
+end
